7982a9ca6fbe8f96943084701bb764f922fbfff8
Aaron Huggins <achuggi1@asu.edu>
2024-08-20 11:41:15 -0700

    Add object modification to page and standardize

--- warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
+++ warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
@@ -3,9 +3,13 @@ package com.skillstorm.testingComponents;
 import java.util.List;
 
 public interface Page {
+    // Each page should only have one item
+    public void modifyObjectRight();
+    public void modifyObjectWrong();
+
     // Each page should enter the information into their own form.
-    public void enterBadFormInformation();
-    public void enterGoodFormInformation(); 
+    public void enterWrongFormInformation();
+    public void enterRightFormInformation(); 
 
     public boolean submitForm(); // Returns success status of the attempt to submit the form (not necessarily success of the form's submission).
 }
