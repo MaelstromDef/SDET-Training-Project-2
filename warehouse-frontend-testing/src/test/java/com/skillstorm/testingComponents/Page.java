b921bf9419f37fc905e1a4dfdd56334f630c3f7f
Aaron Huggins <achuggi1@asu.edu>
2024-08-20 11:13:45 -0700

    fix form info entering

--- warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
+++ warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
@@ -3,6 +3,9 @@ package com.skillstorm.testingComponents;
 import java.util.List;
 
 public interface Page {
-    public void enterFormInformation(List<String> information); // Each page should enter the information into their own form.
+    // Each page should enter the information into their own form.
+    public void enterBadFormInformation();
+    public void enterGoodFormInformation(); 
+
     public boolean submitForm(); // Returns success status of the attempt to submit the form (not necessarily success of the form's submission).
 }
