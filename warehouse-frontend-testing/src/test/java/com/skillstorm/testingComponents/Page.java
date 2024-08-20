153534e74442df643d3691a18f56400e85633a1e
Aaron Huggins <achuggi1@asu.edu>
2024-08-20 12:00:49 -0700

    Remove trailing items from Page

--- warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
+++ warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/Page.java
@@ -3,9 +3,5 @@ package com.skillstorm.testingComponents;
 import java.util.List;
 
 public interface Page {
-    // Each page should enter the information into their own form.
-    public void enterWrongFormInformation();
-    public void enterRightFormInformation(); 
-
-    public boolean submitForm(); // Returns success status of the attempt to submit the form (not necessarily success of the form's submission).
+    
 }
