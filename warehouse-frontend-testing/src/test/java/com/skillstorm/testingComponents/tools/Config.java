cfdb0ee9cbc95b365831746dc57032712327fc9d
Aaron Huggins <achuggi1@asu.edu>
2024-08-21 13:49:46 -0700

    Add data config.

---
+++ warehouse-frontend-testing/src/test/java/com/skillstorm/testingComponents/tools/Config.java
@@ -0,0 +1,6 @@
+package com.skillstorm.testingComponents.tools;
+
+public class Config {
+    public static final String VALID_COMPANY_NAME = "My Valid Company";
+    public static final String VALID_PASSWORD = "myValidPassword";
+}
