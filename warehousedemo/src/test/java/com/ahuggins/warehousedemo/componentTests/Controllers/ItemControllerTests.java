e20761e6c71903c751b929f12c47f014a1b75b40
Bryar-Frank <54441650+Bryar-Frank@users.noreply.github.com>
2024-08-29 17:37:03 -0400

    creates TODO methods to implement for all controller testing

--- warehousedemo/src/test/java/com/ahuggins/warehousedemo/componentTests/Controllers/ItemControllerTests.java
+++ warehousedemo/src/test/java/com/ahuggins/warehousedemo/componentTests/Controllers/ItemControllerTests.java
@@ -1,5 +1,93 @@
 package com.ahuggins.warehousedemo.componentTests.Controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.ahuggins.warehousedemo.controllers.ItemController;
+import com.ahuggins.warehousedemo.models.Item;
+import com.ahuggins.warehousedemo.services.ItemService;
+
+
 public class ItemControllerTests {
+    @Mock 
+    private ItemService itemService;
+
+    @InjectMocks
+    private ItemController itemController;
+
+    private AutoCloseable closeable;
+
+    @BeforeClass
+    public void setup(){
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterClass
+    public void teardown() throws Exception{
+        closeable.close();
+    }
+
+    //#region Data
+
+    @DataProvider(name="ItemList")
+    public Object[][] provideItemList(){
+        return new Object[][] {
+            { new Item(1), new Item(2), new Item(3) }
+        };
+    }
+
+
     
+    @Test(dataProvider = "ItemDtoList")
+    public void getWarehouseItemsTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemList")
+    public void getItemByIdTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemList")
+    public void getItemByNameTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemList")
+    public void addItemToWarehouseTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void updateWarehouseItemTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void removeItemFromWarehouseTest(Item[] items){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
 }
--- warehousedemo/src/test/java/com/ahuggins/warehousedemo/componentTests/Controllers/WarehouseControllerTests.java
+++ warehousedemo/src/test/java/com/ahuggins/warehousedemo/componentTests/Controllers/WarehouseControllerTests.java
@@ -1,5 +1,96 @@
 package com.ahuggins.warehousedemo.componentTests.Controllers;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.ahuggins.warehousedemo.controllers.WarehouseController;
+import com.ahuggins.warehousedemo.models.Item;
+import com.ahuggins.warehousedemo.models.Warehouse;
+import com.ahuggins.warehousedemo.services.WarehouseService;
+
+
 public class WarehouseControllerTests {
+        @Mock 
+    private WarehouseService warehouseService;
+
+    @InjectMocks
+    private WarehouseController warehouseController;
+
+    private AutoCloseable closeable;
+
+    @BeforeClass
+    public void setup(){
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterClass
+    public void teardown() throws Exception{
+        closeable.close();
+    }
+
+    @DataProvider(name="WarehouseList")
+    public Object[][] provideItemList(){
+        return new Object[][] {{
+            new Warehouse(1, "Warehouse Name 1", "Location 1", 1000),
+            new Warehouse(1, "Warehouse Name 2", "Location 2", 2000),
+            new Warehouse(1, "Warehouse Name 3", "Location 3", 3000)
+        }};
+    }
+
+
+    
+    @Test(dataProvider = "ItemDtoList")
+    public void getAllWarehousesTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void getWarehouseByIdTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void getWarehouseByNameTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void getWarehouseByLocationTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void createWarehouseTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+
+    @Test(dataProvider = "ItemDtoList")
+    public void updateWarehouseTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
+    
+    @Test(dataProvider = "ItemDtoList")
+    public void deleteWarehouseTest(Warehouse[] warehouses){
+        //TODO: Setup Mock Information and Methods
+        
+        //TODO: Test All Branches of Method
+    }
     
 }
